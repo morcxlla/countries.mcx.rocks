b0c69dd61549d37013562914caae487f3f1501e3
morcxlla <jorgecividanes2016@gmail.com>
2025-04-21 19:17:02 +0200

    N/A

--- src/app/api/countries/route.ts
+++ src/app/api/countries/route.ts
@@ -2,11 +2,13 @@ import { NextResponse } from 'next/server'
 import countries from '@/data/countries.json'
 
 export function GET() {
-  return NextResponse.json(
+  const res = NextResponse.json(
     countries.sort((a, b) => {
       if (a.UN_observer && !b.UN_observer) return 1
       if (!a.UN_observer && b.UN_observer) return -1
       return a.name.localeCompare(b.name)
     })
   )
+  res.headers.set('Access-Control-Allow-Origin', '*')
+  return res
 }
